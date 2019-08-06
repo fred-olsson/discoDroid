f4c689d899d694e16ec775847af121b3f63fb6fd
fred <olsson.fred@gmail.com>
2019-08-06 23:35:42 +0200

    added permissions check

--- discoDroid/cogs/cogs.py
+++ discoDroid/cogs/cogs.py
@@ -2,6 +2,7 @@
 import logging
 import discord
 from discord.ext import commands
+from discoDroid.constants import is_dev
 
 # Cog handler
 
@@ -12,18 +13,21 @@ class Cogs(commands.Cog):
         self.log = logging.getLogger(__name__)
 
     @commands.command()
+    @commands.check(is_dev)
     async def load(self, ctx, extension):
         self.bot.load_extension(f'discoDroid.cogs.{extension}')
         await ctx.send(f'Loaded cog: {extension}')
         self.log.info(f'Loaded cog: {extension}')
     
     @commands.command()
+    @commands.check(is_dev)
     async def unload(self, ctx, extension):
         self.bot.unload_extension(f'discoDroid.cogs.{extension}')
         await ctx.send(f'Unloaded cog: {extension}')
         self.log.info(f'Unloaded cog: {extension}')
 
     @commands.command()
+    @commands.check(is_dev)
     async def reload(self, ctx, extension):
         self.bot.unload_extension(f'discoDroid.cogs.{extension}')
         self.bot.load_extension(f'discoDroid.cogs.{extension}')
--- discoDroid/constants.py
+++ discoDroid/constants.py
@@ -4,8 +4,15 @@ import json
 
 log = logging.getLogger(__name__)
 
-with open('config.json', mode='r') as f:
+with open('config.json', 'r') as f:
     CONFIG = json.load(f)
 
 LOG_FILE = CONFIG['logging']['handlers']['file_handler']['filename'][1:]
 LOG_DIR = '/' + LOG_FILE.split('/')[1]
+
+def is_dev(ctx):
+    with open('users.json', 'r') as f:
+        users = json.load(f)
+    for user in users['devs']:
+        if ctx.author.id == user:
+            return True
