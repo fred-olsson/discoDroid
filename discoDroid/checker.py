ced9f2f4bba6e53577d196aa20e115d7f313a83d
fred <olsson.fred@gmail.com>
2019-08-07 02:32:38 +0200

    users and checkers updates

--- discoDroid/checker.py
+++ discoDroid/checker.py
@@ -6,8 +6,10 @@ import json
 
 def is_dev(ctx):
     log = logging.getLogger(__name__)
+    
     with open('users.json', 'r') as f:
         users = json.load(f)
+    
     for user in users['devs']:
         if ctx.author.id == user:
             log.info('Dev verified.')
@@ -16,12 +18,13 @@ def is_dev(ctx):
 
 def is_master(ctx):
     log = logging.getLogger(__name__)
+    
+    if is_dev(ctx):
+        return True
+    
     with open('users.json', 'r') as f:
         users = json.load(f)
-    for user in users['devs']:
-        if ctx.author.id == user:
-            log.info('Dev verified.')
-            return True
+    
     for user in users['masters']:
         if ctx.author.id == user:
             log.info('Master verified.')
@@ -30,16 +33,16 @@ def is_master(ctx):
 
 def is_assistant(ctx):
     log = logging.getLogger(__name__)
+    
+    if is_dev(ctx):
+         return True
+    
+    if is_master(ctx):
+         return True
+    
     with open('users.json', 'r') as f:
         users = json.load(f)
-    for user in users['devs']:
-        if ctx.author.id == user:
-            log.info('Dev verified.')
-            return True
-    for user in users['masters']:
-        if ctx.author.id == user:
-            log.info('Master verified.')
-            return True
+    
     for user in users['assistants']:
         if ctx.author.id == user:
             log.info('Assistant verified.')
